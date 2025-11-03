fd5b05a07a20c1e316e1a0e954fbecc385eb6dee
Harsh Yadav <104095087+chintyadav@users.noreply.github.com>
2025-11-03 19:54:03 +0545

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma.vectorstores import Chroma
 
 # Correct chain imports (submodule paths)
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
@@ -243,6 +244,7 @@ if "retriever" in st.session_state:
 
 else:
     st.info("👆 Enter a URL and click 'Load & Process URL' to start.")
+
 
 
 
